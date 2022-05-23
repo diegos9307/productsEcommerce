abe427229ae18d4effd28fee11f622142257e703
diegos9307 <diegos9307@gmail.com>
2022-05-23 10:05:03 -0500

    dandole estilos a detail pag

--- src/components/CardDetail.js
+++ src/components/CardDetail.js
@@ -1,17 +1,42 @@
 import { PropTypes } from 'prop-types';
 
+const capitalizarPrimeraLetra = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
 const CardDetail = ({ detailCard }) => {
   CardDetail.propTypes = {
     detailCard: PropTypes.object
   };
-  console.log(CardDetail);
   return (
-    <div>
-      <div className="card">
-        <img src={detailCard.image} alt="Product" />
-        <div className="card__content">
-          <h4 className="card__title">{detailCard.title}</h4>
+    <div className="detailCard">
+      <div className="detailCard__body">
+        <h2 className="detailCard__title">
+          {detailCard.title.length >= 60 ? detailCard.title.slice(0, 60) : detailCard.title}
+        </h2>
+        <span className="detailCard__description">
+          {detailCard.description.length >= 250
+            ? detailCard.description.slice(0, 250)
+            : detailCard.description}
+        </span>
+        <div>
+          <span className="detailCard__subtitle">Disponibilidad: </span>
+          <span className="detailCard__number">{detailCard.rating.count}</span>
         </div>
+        <div className="detailCard__subline">
+          <div>
+            <span className="detailCard__subtitle">Precio: </span>
+            <span className="detailCard__number">{detailCard.price}</span>
+          </div>
+          <div>
+            <span className="detailCard__subtitle">Rate: </span>
+            <span className="detailCard__number">{detailCard.rating.rate}</span>
+          </div>
+        </div>
+      </div>
+      <div className="detailCard__media">
+        <img src={detailCard.image} alt="Product" />
+        <span className="detailCard__category">{capitalizarPrimeraLetra(detailCard.category)}</span>
       </div>
     </div>
   );
