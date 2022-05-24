6acf9684acb7f569aa0672758baf4e82be15b55b
diegos9307 <diegos9307@gmail.com>
2022-05-24 14:39:44 -0500

    About vista inicial

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,26 +11,39 @@ const About = () => {
           <p className="aboutInfo__item">
             Desarrollador Web Full Stack, Ingeniero Mecánico. Disfruto trabajar en equipo y resolver
             problemas para ayudar a las organizaciones a superar sus desafíos a través de la
-            tecnología.
+            tecnología. Creó que estamos viviendo una revolución digital y quiero hacer parte de
+            está.
+            <br /> Me gustaría aprender sobre blockchain y web 3.0, luego de afianzar mis
+            conocimientos como desarrollador full stack ese sería el paso siguiente. En mi tiempo
+            libre me gusta compartir con mi familia y amigos, salir a comer algo rico, leer, hacer
+            ejercicio salir y caminar para interactuar con la naturaleza.
           </p>
-          <ul className="aboutInfo__item">
-            <li>En este programa pude entender de manera practica como es el uso de Gitflow.</li>
-            <li>
-              En este proyecto pude usar react router de manera practica, entender mucho mejor el
-              uso de los hooks y principalmente context
-            </li>
-            <li>
-              En este proyecto aprendi a hacer un sitio web full stack entendiendo como se conecta
-              el front-end en react con el back-end usando node.js
-            </li>
-          </ul>
-          <div className="aboutInfo__link">
-            <a href="mailto:diegos9307@gmail.com">
+          <p className="aboutInfo__item">
+            <i className="fa-solid fa-code"></i>En este programa pude entender de manera práctica
+            como es el uso de Gitflow.
+          </p>
+          <p className="aboutInfo__item">
+            <i className="fa-solid fa-code"></i>En este proyecto pude usar react router de manera
+            práctica, entender mucho mejor el uso de los hooks y principalmente context.
+          </p>
+          <p className="aboutInfo__item">
+            <i className="fa-solid fa-code"></i>En este proyecto aprendí a hacer un sitio web full
+            stack entendiendo como se conecta el front-end en react con el back-end usando node.js.
+          </p>
+          <h3 className="aboutInfo__subtitle">Contacto</h3>
+          <div className="aboutInfo__subcontent">
+            <a
+              href="mailto:diegos9307@gmail.com"
+              className="aboutInfo__link"
+              target="_blank"
+              rel="noopener noreferrer">
               <i className="fa-solid fa-envelope"></i> diegos9307@gmail.com
             </a>
-          </div>
-          <div className="aboutInfo__link">
-            <a href="https://github.com/diegos9307">
+            <a
+              href="https://github.com/diegos9307"
+              className="aboutInfo__link"
+              target="_blank"
+              rel="noopener noreferrer">
               <i className="fa-brands fa-github"></i> GitHub
             </a>
           </div>
